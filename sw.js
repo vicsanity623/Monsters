d048dcc86bcc69397cd351a43a9f6271dca08299
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-01-31 23:40:04 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'goku-rpg-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './IMG_0054.png',
+  './IMG_0061.png',
+  './IMG_0062.png'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache if available, otherwise fetch from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
